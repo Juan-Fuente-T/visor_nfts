16ea76e65c0b741052d38fa24ede9fb4119b3cf9
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-07-20 14:34:13 +0200

    First commit. App minteando Business Card NFTs.

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// export const CONTRACT_ADDRESS = '0x48177f98d8Bab403c5B339370515A8b6b6b62A1B'
+// export const CONTRACT_ADDRESS = '0xeE054da1f69EEB4953e4757Ad0b7Fd006a53b58C'
+export const CONTRACT_ADDRESS = '0x5BC4F8626E434F3f209Bcb7fB002e7003716e586' //1155 sin autentication , sin onlyowwer en Sepolia para BusinessCard
+
